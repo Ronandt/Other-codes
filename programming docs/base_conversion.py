3f494320f21db3f477e6bd155e1ebf4e9bc02cbf
Isaac <49837565+Ronandt@users.noreply.github.com>
2021-05-14 19:03:20 +0800

    changes to efficiency

--- programming docs/base_conversion.py
+++ programming docs/base_conversion.py
@@ -1,7 +1,7 @@
 while 1:
     try:
         y = float(input("What Number (Press any character to quit): "))
-        u = int(input("What base do you want to convert from?"))
+        #u = int(input("What base do you want to convert from?"))
         v = int(input("What base (Up to 16) do you want to convert to: "))
     except ValueError:
         print("Quit!")
@@ -9,9 +9,10 @@ while 1:
 
     f = []
     p = []
+    k = ""
     dict = {
-        "10": "A",
-        "11": "B",
+        "10" : "A",
+        "11" : "B",
         "12": "C",
         "13": "D",
         "14": "E",
@@ -24,18 +25,18 @@ while 1:
             p.append(str(int(b // 1)))
             b = b % 1 * v
         if v <= 10:
-            o = "".join(p)
+            k += "".join(p) + "."
         elif 10 < v <= 16:
-            o = "".join([dict[x] if int(x) >= 10 else x for x in p])
+            k += "".join([dict[x] if int(x) >= 10 else x for x in p]) + "."
         else:
             pass
     while y != 0:
         f.append(str(int(y % v)))
         y //= v
     if v <= 10:
-        n = "".join(f)[::-1]
-    elif 10 < v <= 16:
-        n = "".join([dict[x] if int(x) >= 10 else x for x in f][::-1])
+        k += "".join(f)[::-1]
+    elif 10 < v <=16:
+        k += "".join([dict[x] if int(x) >= 10 else x for x in f][::-1])
     else:
         print("Invalid Input!")
-    print(n + "." + o)
+    print(k)
