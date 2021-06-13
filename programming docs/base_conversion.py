03b3975bbfbaae20a2a2505a7bfbd0375e0222de
Isaac <49837565+Ronandt@users.noreply.github.com>
2021-06-13 21:30:12 +0800

    wtf is going on

--- programming docs/base_conversion.py
+++ programming docs/base_conversion.py
@@ -1,49 +1,51 @@
 import sys
-while 1:
-    try:
-        y = float(input("What Number (Press any character to quit): "))
-        #u = int(input("What base do you want to convert from?"))
-        v = int(input("What base (Up to 16) do you want to convert to: "))
-        z = int(input("What is the precision you want to set it to: "))
-        if z < 0:
-            print("Precision cannot be negative!")
-            sys.exit()
-    except ValueError:
-        print("Quit!")
-        break
-    n = '0'
-    f = []
-    p = []
-    k = ""
-    dict = {
-        "10" : "A",
-        "11" : "B",
-        "12": "C",
-        "13": "D",
-        "14": "E",
-        "15": "F",
-    }
-    if y % 1 != 0:
-        b = y % 1 * v
-        y -= y % 1
-        for l in range(z):
-            p.append(str(int(b // 1)))
-            b = b % 1 * v
+def hexConverter():
+    while 1:
+        try:
+            y = float(input("What Number (Press any character to quit): "))
+            v = int(input("What base (Up to 16) do you want to convert to: "))
+            z = int(input("What is the precision you want to set it to: "))
+            if z < 0:
+                print("Precision cannot be negative!")
+                sys.exit()
+        except ValueError:
+            print("Quit!")
+            break
+        n, f, p, k = '0', [], [], ""
+        dict = {
+            "10" : "A",
+            "11" : "B",
+            "12": "C",
+            "13": "D",
+            "14": "E",
+            "15": "F",
+        }
+        c = abs(y)
+        if c % 1 != 0:
+            b = c % 1 * v
+            c -= c % 1
+            for l in range(z):
+                p.append(str(int(b // 1)))
+                b = b % 1 * v
+            if v <= 10:
+                n = "".join(p)
+            elif 10 < v <= 16:
+                n = "".join([dict[x] if int(x) >= 10 else x for x in p])
+            else:
+                pass
+        while y != 0:
+            f.append(str(int(y % v)))
+            c //= v
         if v <= 10:
-            n = "".join(p) 
-        elif 10 < v <= 16:
-            n = "".join([dict[x] if int(x) >= 10 else x for x in p]) 
+            o = "".join(f)[::-1]
+        elif 10 < v <=16:
+            o = "".join([dict[x] if int(x) >= 10 else x for x in f][::-1])
         else:
-            pass
-    while y != 0:
-        f.append(str(int(y % v)))
-        y //= v
-    if v <= 10:
-        o = "".join(f)[::-1]
-    elif 10 < v <=16:
-        o = "".join([dict[x] if int(x) >= 10 else x for x in f][::-1])
-    else:
-        print("Invalid Input!")
-        sys.exit()
+            print("Invalid Input!")
+            sys.exit()
 
-    print(o + "." +  n)
+        print((y < 0) * "-" + o + "." + n)
+
+
+
+print(hexConverter())
