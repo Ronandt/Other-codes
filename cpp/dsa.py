5323c0e8d71d51bedd7502afbc5e07f8ada34db5
Isaac <49837565+Ronandt@users.noreply.github.com>
2022-04-18 18:25:48 +0800

    fff

--- cpp/dsa.py
+++ cpp/dsa.py
@@ -66,7 +66,10 @@ class CreditCard:
 if __name__ == "__main__":
     wallet = []
     wallet.append(CreditCard("John Lee", "DBS", "2032839232", 2500))
-
+    wallet.append(CreditCard('John Lee', 'OCBC',
+    '3485 0399 3395 1954', 3500) )
+    wallet.append(CreditCard('John Lee', 'Maybank',
+    '5391 0375 9387 5309', 5000) )
     for val in range(1, 17):
         wallet[0].charge(val)
         wallet[1].charge(2 * val)
@@ -76,7 +79,8 @@ if __name__ == "__main__":
         print("Customer =", wallet[c].get_customer())
         print("Bank = ", wallet[c].get_bank())
         print("Account = ", wallet[c].get_account())
-        print("Lijmit = ", wallet[c].get_balance())
+        print("Limit = ", wallet[c].get_balance())
         while wallet[c].get_balance() > 100:
             wallet[c].make_payment(100)
-            print("new blanace =", wallet[c].get_balance)
+            print("new balance =", wallet[c].get_balance())
+        print()
--- cpp/dsa2.py
+++ cpp/dsa2.py
@@ -9,16 +9,33 @@ class PredatoryCreditCard(CreditCard):
 
     def charge(self, price):
         if price + self.get_balance() > self.get_limit():
-            self.__balance -= 5
+            self.make_payment(5)
             return False
         else:
-            self.__balance += price
+            self.make_payment(-5)
             return True
 
     def process_month(self):
-        return (1- ((self.__apr + 1)**(1/12))) * self.__balance
+        return (1- ((self.__apr + 1)**(1/12))) * self.get_balance()
 
-objects = PredatoryCreditCard("DFDS","DBS", "438 238 193 391", 2500, 1.2944 )
-
-objects.charge(2499)
-objects.process_month()
+if __name__ == "__main__":
+    wallet = []
+    wallet.append(CreditCard("John Lee", "DBS", "2032839232", 2500))
+    wallet.append(CreditCard('John Lee', 'OCBC',
+    '3485 0399 3395 1954', 3500) )
+    wallet.append(CreditCard('John Lee', 'Maybank',
+    '5391 0375 9387 5309', 5000) )
+    for val in range(1, 17):
+        wallet[0].charge(val)
+        wallet[1].charge(2 * val)
+        wallet[2].charge(3 * val)
+    
+    for c in range(3):
+        print("Customer =", wallet[c].get_customer())
+        print("Bank = ", wallet[c].get_bank())
+        print("Account = ", wallet[c].get_account())
+        print("Limit = ", wallet[c].get_balance())
+        while wallet[c].get_balance() > 100:
+            wallet[c].make_payment(100)
+            print("new balance =", wallet[c].get_balance())
+        print()
